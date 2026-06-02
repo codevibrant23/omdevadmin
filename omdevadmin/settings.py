d9f81217ea6d8fe8fbb7e518a428cc0e4900f445
AnuragJha1954 <anuragjha@jklu.edu.in>
2026-06-03 03:02:27 +0530

    configure for koyeb + neon deployment

--- omdevadmin/settings.py
+++ omdevadmin/settings.py
@@ -80,13 +80,19 @@ WSGI_APPLICATION = 'omdevadmin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=f"sqlite:///{BASE_DIR}/db.sqlite3",
+#         conn_max_age=600
+#     )
+# }
 DATABASES = {
     'default': dj_database_url.config(
         default=f"sqlite:///{BASE_DIR}/db.sqlite3",
-        conn_max_age=600
+        conn_max_age=600,
+        conn_health_checks=True,
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
